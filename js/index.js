e632c6d5e96393f94c4461652d24644d8c1ed97b
Walter Liendo <walter.liendo@gmail.com>
2023-04-21 20:24:43 -0300

    cree los diccionarios para encriptar y desencriptar

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// Dicionario para encriptar
+const e = text => {
+    return text.replace(/e/g, "enter")
+                .replace(/i/g, "imes")
+                .replace(/a/g, "ai")
+                .replace(/o/g, "ober")
+                .replace(/u/g, "ufat")
+}
+
+//Dicionario para desencriptar
+const d = text => {
+    return text.replace(/enter/g, "e")
+                .replace(/imes/g, "i")
+                .replace(/ai/g, "a")
+                .replace(/ober/g, "o")
+                .replace(/ufat/g, "u")
+}
