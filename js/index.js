f760f7ea1c9caa3dd24f024aba73f58d74599b3b
Walter Liendo <walter.liendo@gmail.com>
2023-04-21 22:11:15 -0300

    no funciona aun

--- js/index.js
+++ js/index.js
@@ -1,17 +1,90 @@
 // Dicionario para encriptar
-const e = text => {
-    return text.replace(/e/g, "enter")
-                .replace(/i/g, "imes")
-                .replace(/a/g, "ai")
-                .replace(/o/g, "ober")
-                .replace(/u/g, "ufat")
+const diccionario = {
+    'e': 'enter',
+    'i': 'imes',
+    'a': 'ai',
+    'o': 'ober',
+    'u': 'ufat'
+};
+
+function encriptar(text){
+    let resultado = '';
+
+    for (let i = 0; i < text.length; i++) {
+        const char = text[i];
+
+        if (diccionario[char]) {
+            resultado += diccionario[char];
+        } else {
+            resultado += char;
+        }
+    }
+    return resultado;
+}
+const pattern = /^[a-z\s]*$/;
+
+function handleEncryption() {
+    const inputText = document.getElementById('message').value.toLowerCase();
+
+    if (!pattern.test(inputText)) {
+        document.getElementById('error').textContent = 'Ingrese solo letras minúsculas y sin acentos';
+        document.getElementById('resultado').textContent = '';
+        return;
+    }
+    const textoEncriptado = encriptar(inputText);
+
+    document.getElementById('resultado').textContent = textoEncriptado;
+    document.getElementById('error').textContent = '';
+
 }
 
-//Dicionario para desencriptar
-const d = text => {
-    return text.replace(/enter/g, "e")
-                .replace(/imes/g, "i")
-                .replace(/ai/g, "a")
-                .replace(/ober/g, "o")
-                .replace(/ufat/g, "u")
+function desencriptar(textoEncriptado){
+    let resultado = '';
+
+    //Reemplazamos cada caracter encriptado por su letra original
+    for (let i = 0; i < textoEncriptado.length;) {
+        const substring = textoEncriptado.substring(i);
+        const match = substring.match(/^(enter|imes|ai|ober|ufat)/);
+    
+    if (match) {
+        resultado += Object.keys(diccionario)
+        .find(key => diccionario[key] === match[0]);
+        i += match[0].length;
+    } else {
+        resultado += substring[0];
+        i++;
+    }
+    
+
+
+// Probando si funciona 
+// const textoEncriptado = encriptar('hola');
+// console.log(textoEncriptado);
+// const textoDesencriptado = desencriptar(textoEncriptado);
+// console.log(textoDesencriptado);
+    }}
+// Copiando resultado en el portapapeles
+async function copiarEnPortapapeles() {
+    let text = document.getElementById('texto').innerHTML;
+    try {
+        await navigator.clipboard.writeText(text);
+        // mostrar mensaje de copiado
+        console.log("Texto copiado al portapapeles" + text);
+    } catch (err) {
+        // mostrar mensaje de no copiado
+        console.error("Error al copiar texto al portapapeles:", err);
+    }
+
+// let resultadoCopiado = document.getElementById('resultado');
+
+// resultadoCopiado.value = resultado;
+// resultadoCopiado.style.display = 'block';
+
+// navigator.clipboard.writeText(resultado)
+// .then(() => console.log('Texto copiado'))
+// .catch(err => console.log('Error al copiar el texto: ', err));
+//     }
+// return resultado;
 }
+
+
